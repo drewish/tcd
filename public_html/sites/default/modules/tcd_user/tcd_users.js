419f9e06e60053b554b542ee1d7d4eac05473a72
Eric J. Duran <eric.duran7@gmail.com>
2011-05-21 22:02:44 -0400

    Fixing iphone like password

--- public_html/sites/default/modules/tcd_user/tcd_users.js
+++ public_html/sites/default/modules/tcd_user/tcd_users.js
@@ -4,7 +4,7 @@
      // first lets remove the default behaviors for the password field.
      Drupal.detachBehaviors($('#user-register-form .password-field'), settings);
      // Attached our iphone like password.
-     $('.form-type-password input:password.password-field').dPassword({
+     $('.form-type-password input:password').dPassword({
         duration: 1000
       });
     }
