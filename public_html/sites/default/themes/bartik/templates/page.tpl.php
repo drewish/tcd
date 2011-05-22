73df041af3ee0ff420f501357a05381fdaca8c57
Eric J. Duran <eric.duran7@gmail.com>
2011-05-22 05:19:36 -0400

    Adding dynamic-font classes

--- public_html/sites/default/themes/bartik/templates/page.tpl.php
+++ public_html/sites/default/themes/bartik/templates/page.tpl.php
@@ -176,7 +176,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1 class="title" id="page-title">
+        <h1 class="title dynamic-font" id="page-title">
           <?php print $title; ?>
         </h1>
       <?php endif; ?>
