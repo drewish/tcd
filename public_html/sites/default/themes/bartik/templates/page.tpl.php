0d1226e0e2335a0b7bcb7f10ceda7b00eedd5142
Shariq Hussain <shariqhussain@shariq-hussains-macbook-3.local>
2011-05-21 23:37:53 -0700

    form and background defaults theme markup

--- public_html/sites/default/themes/bartik/templates/page.tpl.php
+++ public_html/sites/default/themes/bartik/templates/page.tpl.php
@@ -89,11 +89,7 @@
 
   <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
+   
 
     <?php if ($site_name || $site_slogan): ?>
       <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
@@ -112,11 +108,7 @@
           <?php endif; ?>
         <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
+       
 
       </div> <!-- /#name-and-slogan -->
     <?php endif; ?>
