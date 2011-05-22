2a68a41e1073360ab49f6d964130306ed58c0423
Eric J. Duran <eric.duran7@gmail.com>
2011-05-21 21:56:48 -0400

    Removing confirmation password field

--- public_html/sites/default/modules/tcd_user/tcd_users.js
+++ public_html/sites/default/modules/tcd_user/tcd_users.js
@@ -2,11 +2,7 @@
   Drupal.behaviors.tcd_users = {
     attach: function (context, settings) {
      // first lets remove the default behaviors for the password field.
-     $('.password-strength').remove();
-     $('.password-suggestions').remove();
-     $('input.password-confirm').remove();
      Drupal.detachBehaviors($('#user-register-form .password-field'), settings);
-
      // Attached our iphone like password.
      $('.form-type-password input:password.password-field').dPassword({
         duration: 1000
